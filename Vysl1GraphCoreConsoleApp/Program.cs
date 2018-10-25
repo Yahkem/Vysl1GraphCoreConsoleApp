0e8ee09ba93219b4c0c4a566da38be9af35a4c10
Richard Kubný <p17083@student.osu.cz>
2018-10-25 12:51:23 +0200

    Add project files.

---
+++ Vysl1GraphCoreConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Vysl1GraphCoreConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
